cb1c9e0f86516611a0b8d672f2e657e1d02828a8
meldy2503 <emelder.charles25@gmail.com>
2022-02-09 01:54:44 -0800

    add section3 content

--- src/components/HomePage/Section_3/Section3Style.js
+++ src/components/HomePage/Section_3/Section3Style.js
@@ -17,7 +17,39 @@ export const Title = styled.div`
       width: 43%;
       margin: 0 auto; 
       line-height: 1.3;
-      padding: 1.3rem 0;
-      
+      padding: 1.3rem 0;  
    }
 `;
+
+export const Container = styled.div`
+     max-width: 120rem;
+     width: 90%;
+     margin: 0 auto;
+`;
+export const Contents = styled.div`
+     height: 30rem;
+     width: 40rem;
+     background-color: #fff;
+     box-shadow: .1rem .2rem .3rem #d0d0d0;
+     margin-top: 4rem;
+   
+   div { 
+     width: 80%;
+     margin: 0 auto;
+     padding: 5rem 0;
+   
+     h6 {
+      font-size: 2.3rem;
+      margin: 1rem 0;
+      font-weight: 600;   
+   }
+   
+   p {
+      line-height: 1.8;
+      
+     }
+
+
+   }
+`;
+
--- src/components/HomePage/Section_3/index.js
+++ src/components/HomePage/Section_3/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MainContainer } from "../../reusableComponents/ContainerStyled";
-import { Title } from "../Section_3/Section3Style";
+import { Title, Container, Contents } from "../Section_3/Section3Style";
+import icon1 from "../../images/Icon1.svg";
 
 function Section3() {
   return (
@@ -9,6 +10,16 @@ function Section3() {
                   <p>Features</p>
                   <h2>Design that solves problems, one product at a time</h2>
               </Title> 
+
+             <Container>
+                  <Contents>
+                     <div>
+                         <img src={icon1} alt="" />
+                         <h6>Uses Client First</h6>
+                         <p>Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam sed faucib turpis eu gravida mi. Pellentesque et velit aliquam sed mi. </p> 
+                     </div>
+                  </Contents>
+             </Container>
 
         </MainContainer>
   );
