8614b4495566887be5f4179b17ce03518d8a375d
meldy2503 <emelder.charles25@gmail.com>
2022-02-09 09:02:23 -0800

    add section 3 mediaquery

--- src/components/HomePage/Section_3/Section3Style.js
+++ src/components/HomePage/Section_3/Section3Style.js
@@ -5,16 +5,27 @@ export const Title = styled.div`
    text-align: center;
    max-width: 120rem;
    width: 90%;
-   margin: 0 auto;  
-   padding: 10rem 0;
+   margin: 0 auto; 
+   padding: 10rem 0 3rem 0; 
+  
    
    h2 {
       font-size: 3.6rem;
       font-weight: 600;
-      width: 40%;
+      width: 45%;
       margin: 0 auto; 
       line-height: 1.3;
-      padding: 1.3rem 0;  
+      padding: 1.3rem 0;
+      
+      @media screen and (max-width: ${({theme}) => theme.mediaQuery.tablet}) {
+         width: 70%;
+         font-size: 3.3rem;
+      } 
+
+      @media screen and (max-width: ${({theme}) => theme.mediaQuery.tablet}) {
+         font-size: 2.8rem;
+         width: 75%;
+      } 
    }
 `;
 
@@ -25,26 +36,52 @@ export const Container = styled.div`
      display: flex;
      flex-wrap: wrap;
      flex-direction: row;
-     row-gap: 9rem;
+     row-gap: 2.5rem;
      justify-content: space-between;
+     padding-bottom: 10rem;
+
+     @media screen and (max-width: ${({theme}) => theme.mediaQuery.mobile}) {
+      row-gap: 2rem;
+      }  
 `;
 export const Contents = styled.div`
      width: 32%;
      background-color: #fff;
      box-shadow: .1rem .2rem .3rem #eee;
-     margin-top: -7rem;
+     /* margin-top: -7rem; */
    
-    
+     @media screen and (max-width: ${({theme}) => theme.mediaQuery.tablet}) {
+         width: 48.5%;
+      } 
+      
+      @media screen and (max-width: ${({theme}) => theme.mediaQuery.mobile}) {
+         width: 90%;
+         margin: 0 auto;
+         /* margin-top: -7rem; */
+         
+      } 
    
    div { 
      width: 83%;
      margin: 0 auto;
      padding: 6rem 0 5rem 0;
+
+     @media screen and (max-width: ${({theme}) => theme.mediaQuery.mobile}) {
+         width: 75%;
+         
+         
+      } 
    
      h6 {
       font-size: 2.2rem;
       margin: 1rem 0;
-      font-weight: 600;   
+      font-weight: 600;  
+      
+      @media screen and (max-width: ${({theme}) => theme.mediaQuery.mobile}) {
+         font-size: 2rem;
+         
+         
+      } 
    }
    
    p {
