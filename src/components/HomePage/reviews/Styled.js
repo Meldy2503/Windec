46d86529451b1ad822e762dfff99ceb7b965cf39
meldy2503 <emelder.charles25@gmail.com>
2022-02-11 17:10:38 +0100

    add left content to  review component

--- src/components/HomePage/reviews/Styled.js
+++ src/components/HomePage/reviews/Styled.js
@@ -1,15 +1,22 @@
 import styled from "styled-components";
 
 export const LeftContent = styled.div`
-       width: 35%;
+       width: 30%;
        display: flex;
        flex-direction: column;
        justify-content: space-between;
 
+       h3 {
+           font-size: 3rem;
+           font-weight: 600;
+           line-height: 1.3;
+           margin-bottom: 1.7rem;
+       }
+
 `;
 
 export const RightContent = styled.div`
-      width: 55%;
+      width: 60%;
       display: flex;
       flex-direction: column;
       justify-content: space-between;
