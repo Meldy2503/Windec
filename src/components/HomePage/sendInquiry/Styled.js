709fbb9ad3cd809ddc73ed581969ea692be56600
meldy2503 <emelder.charles25@gmail.com>
2022-02-13 14:32:50 +0100

    add mediaquery

--- src/components/HomePage/sendInquiry/Styled.js
+++ src/components/HomePage/sendInquiry/Styled.js
@@ -5,25 +5,42 @@ import img6 from "../../images/img6.svg";
 export const LeftContent = styled.div`
   display: flex;
   flex-direction: column; 
-  width: 50%;
+  width: 49%;
   position: relative;
   background-image: linear-gradient(45deg, rgba(28, 30, 83, 0.5), rgba(28, 30, 83, 0.6)), url(${img6});
   /* height: 70rem; */
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
+
+  @media screen and (max-width: ${({theme}) => theme.mediaQuery.tablet}) {
+     width: 45%; 
+  }
+
+  @media screen and (max-width: ${({theme}) => theme.mediaQuery.mobile}) {
+    display: none;
+ } 
          
     div {
          position: absolute;
          top: 8rem;
-         left: 7rem;
+         left: 5rem;
          width: 75%; 
+
+         @media screen and (max-width: ${({theme}) => theme.mediaQuery.tablet}) {
+          left: 3rem;
+          width: 80%; 
+    } 
          
   h1 {
      font-size: 4rem;
      line-height: 1.3; 
      font-weight: 600;
      color: #fff;
+
+     @media screen and (max-width: ${({theme}) => theme.mediaQuery.tablet}) {
+      font-size: 3rem;
+    }   
   }
 
   p {
@@ -35,15 +52,28 @@ export const LeftContent = styled.div`
 `;
 
 export const RightContent = styled.div`
-  width: 50%;
+  width: 51%;
   background-color: ${({theme}) => theme.colors.primary2};
   display: flex;
   flex-direction: column;
+
+  @media screen and (max-width: ${({theme}) => theme.mediaQuery.tablet}) {
+          width: 55%; 
+    } 
+
+  @media screen and (max-width: ${({theme}) => theme.mediaQuery.mobile}) {
+    width: 100%;
+    margin: 0 auto;
+      } 
 
   div {
     width: 70%;
     margin: 0 auto;
     margin-top: 8rem;
+
+    @media screen and (max-width: ${({theme}) => theme.mediaQuery.tablet}) {
+          width: 80%; 
+    } 
 
     h5 {
       color: #fff;
@@ -69,10 +99,18 @@ export const RightContent = styled.div`
       margin-bottom: 1.5rem;
       padding: 2rem;
       border-radius: .5rem;
+      outline: none;
+      color: #fff;
 
       &::placeholder {
         color: ${({theme}) => theme.colors.primary5};
         opacity: 0.6;
+      }
+
+      &:focus {
+        background-color: #F4F6FC33;
+        border-bottom: 2px solid #fff;
+      
       }
     }
   }
@@ -83,21 +121,13 @@ export const RightContent = styled.div`
          color: #fff;
          justify-content: center;
          margin-bottom: 6rem;
-  
-         /* @media screen and (max-width: ${({theme}) => theme.mediaQuery.tablet}) {
-           margin-bottom: 4rem;
-          }  */
-        } 
+     } 
         
         li a {
           text-decoration: none;
           margin-right: 1.5rem;
           align-items: center;
           color: #fff;
-          
-         /* @media screen and (max-width: ${({theme}) => theme.mediaQuery.tablet}) {
-          margin-right: .5rem;
-      }  */
      }
-} 
+   } 
 `;
--- src/components/HomePage/sendInquiry/index.js
+++ src/components/HomePage/sendInquiry/index.js
@@ -13,14 +13,12 @@ function SendInquiry() {
   <MainContainer bgco= "#fff">
       <ContentContainer pt= "0">
           <LeftContent>
-              {/* <img src={img6} alt="bg-img" /> */}
               <div>
-              <h1>
-              Building stellar websites for early startups
-              </h1>
-              <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim.
-              </p>
+                  <h1>
+                  Building stellar websites for early startups
+                  </h1>
+                  <p>Lorem ipsum dolor sit amet,   consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim.
+                  </p>
               </div>
           </LeftContent>
           <RightContent>
