af4687d2e078d9f632a1982f21af155a171e95e2
meldy2503 <emelder.charles25@gmail.com>
2022-02-18 11:00:57 +0100

    create ourProcess section

---
+++ src/components/aboutUs/ourProcess/Data.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const data = [
+    {   
+        id: 1,
+        title: "Uses Client First",
+        text: "Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam sed faucib turpis eu gravida mi. Pellentesque et velit aliquam sed mi."
+    },
+  
+    { 
+      id: 2,
+      title: "Two Free Revision Round",
+      text: "Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam sed faucib turpis eu gravida mi. Pellentesque et velit aliquam sed mi."
+  
+  },
+  
+  {   
+      id: 3,
+      title: "Template Customization",
+      text: "Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam sed faucib turpis eu gravida mi. Pellentesque et velit aliquam sed mi."
+  
+  },
+  
+  {  
+      id: 4,
+      title: "24/7 Support",
+      text: "Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam sed faucib turpis eu gravida mi. Pellentesque et velit aliquam sed mi."
+  
+  }
+  
+  ]
+  
+  export default data;
