7c83964ba268c3c156e411a2bb29e1bbb168adc3
meldy2503 <emelder.charles25@gmail.com>
2022-02-18 01:04:03 +0100

    create whoWeAre section

---
+++ src/components/aboutUs/whoWeAre/Styled.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { LeftContent, RightContent } from "./Styled";
+import { MainContainer, ContentContainer } from "../../reusableComponents/ContainerStyled";
+import img10 from "../../images/img10.svg";
+
+
+
+function Intro() {
+  return (
+  <MainContainer bgco= "#fff">
+      <ContentContainer pt= "14rem">
+          <LeftContent>
+              <div>
+              <h6>
+                  About us
+              </h6>
+              <h1>
+                   Our designs solve problems
+              </h1>
+               <p>
+                  Lorem ipsum dolor sit amet, consect etur adipi scing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+               </p>
+              </div>
+          </LeftContent>
+          <RightContent>
+          <div>
+              <h6>
+                  About us
+              </h6>
+              <h1>
+                   Our designs solve problems
+              </h1>
+               <p>
+                  Lorem ipsum dolor sit amet, consect etur adipi scing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+               </p>
+              </div>
+          </RightContent>
+      </ContentContainer>
+  </MainContainer>
+  )
+}
+
+export default Intro;
+
---
+++ src/components/aboutUs/whoWeAre/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { LeftContent, RightContent } from "./Styled";
+import { MainContainer, ContentContainer } from "../../reusableComponents/ContainerStyled";
+import img10 from "../../images/img10.svg";
+
+
+
+function Intro() {
+  return (
+  <MainContainer bgco= "#fff">
+      <ContentContainer pt= "14rem">
+          <LeftContent>
+              <div>
+              <h6>
+                  About us
+              </h6>
+              <h1>
+                   Our designs solve problems
+              </h1>
+               <p>
+                  Lorem ipsum dolor sit amet, consect etur adipi scing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+               </p>
+              </div>
+          </LeftContent>
+          <RightContent>
+                <img src={img10} alt="img1" />
+          </RightContent>
+      </ContentContainer>
+  </MainContainer>
+  )
+}
+
+export default Intro;
+
