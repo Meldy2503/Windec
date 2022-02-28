54f7641f87fe812987f0910162dfe9d8c9de6a1e
meldy2503 <emelder.charles25@gmail.com>
2022-02-28 11:11:29 +0100

    update animations in IntroPricing  section

--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Intro from "../components/pricing/intro/index";
-
+import PricingFAQ from "../components/pricing/pricingFAQ/index";
 
 
 const Pricing = () => {
    return (
        <div>
            <Intro />
+           <PricingFAQ />
        </div>
    )
 }
