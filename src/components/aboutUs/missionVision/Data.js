d85e07cd0f1c883383f48b391ccfb8f308f2531a
meldy2503 <emelder.charles25@gmail.com>
2022-03-06 12:01:39 +0100

    add text contents to about-us page

--- src/components/aboutUs/missionVision/Data.js
+++ src/components/aboutUs/missionVision/Data.js
@@ -1,24 +1,22 @@
-
 import img12 from "../../images/img12.svg";
 import img13 from "../../images/img13.svg";
 
 const data = [
-    {   
-        id: 1,
-        header: "Our Mission",
-        title: "Inspire,   Innovate, Share",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        img: img12
-    },
-  
-    { 
-      id: 2,
-      header: "Our Vision",
-      title: "Laser focus",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      img: img13
-  }
-  
-  ]
-  
-  export default data;
+  {
+    id: 1,
+    header: "Our Mission",
+    title: "Inspire,   Innovate, Share",
+    text: "Windec was established to help individuals grow their businesses by helping them develop a stronger brand image. Our services detailed and are frequently less expensive and we provide the best of our efforts to all of our clients.",
+    img: img12,
+  },
+
+  {
+    id: 2,
+    header: "Our Vision",
+    title: "Laser focus",
+    text: "Windec is a well-known brand. We have been able to establish in a short period of time, a strong reputation for providing high-quality work. Our vision is to provide our clients with websites that will set them apart from their competitors.",
+    img: img13,
+  },
+];
+
+export default data;
